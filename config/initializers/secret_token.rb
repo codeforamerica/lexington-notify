6659b17ae0de1f54a6a3ebaa7c3977beecb4abbb
Danny Whalen <daniel.r.whalen@gmail.com>
2014-01-13 09:45:57 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfaRails::Application.config.secret_key_base = 'fc42b7e7c6edb59d0fc5fe55febb2aa9c0ff139883d3bd1abd14e4f5db393fa79043df3d9c157cc90569ec3a6dec835e08b77bda8328bf3dbaddd9a87c17846f'

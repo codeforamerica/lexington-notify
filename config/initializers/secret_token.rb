491d582d1ba1693044ef594db278b2ff10299a03
Danny Whalen <daniel.r.whalen@gmail.com>
2014-01-13 17:57:19 -0800

    Extract secret key base into environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-CfaRails::Application.config.secret_key_base = 'fc42b7e7c6edb59d0fc5fe55febb2aa9c0ff139883d3bd1abd14e4f5db393fa79043df3d9c157cc90569ec3a6dec835e08b77bda8328bf3dbaddd9a87c17846f'
+CfaRails::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']

df592dfbc212f1d2dad6a8231df12665a6b0ce06
Erik Schwartz <erik@erikschwartz.net>
2014-03-14 13:26:21 -0700

    Add test for sending an sms

--- spec/features/admin_sends_sms_spec.rb
+++ spec/features/admin_sends_sms_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
-describe 'foo' do
-  it 'foo' do
-    expect(true).to be_true
+describe 'admin visits new notification page' do
+  it 'sends message' do
+    visit new_notifications_path
+    fill_in 'Number', with: '3128600685'
+    fill_in 'Message', with: 'Hello from test env!'
+    click_button 'Submit'
+    expect(page).to have_content('sent to')
   end
 end

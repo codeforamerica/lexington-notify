db993826b94930b35c70994a63f6c8c2b5cb0a7a
Lyzi Diamond <lyzidiamond@gmail.com>
2014-03-28 16:20:02 -0700

    added nominatim geocoder

--- js-pip/js/trashDay.js
+++ js-pip/js/trashDay.js
@@ -1,19 +1,32 @@
 var quadToDay = {
-			"A": "Monday",
-			"B": "Thursday",
-			"C": "Tuesday",
-			"D": "Friday",
-			"E": "Daily"
-		};
+	"A": "Monday",
+	"B": "Thursday",
+	"C": "Tuesday",
+	"D": "Friday",
+	"E": "Daily"
+};
 
-		function onLocationFound(ll) {
-			var layer = leafletPip.pointInLayer(ll, quads);
-			var quad = layer[0].feature.properties.QUAD;
-			var pickupDay = quadToDay[quad];
+var quads = L.geoJson(quadsJson);
 
-			document.getElementById('day').innerHTML = layer.length ? '<h2>Your trash gets picked up on ' + pickupDay + '</h2>' : '';
-		};
-	
-		var quads = L.geoJson(quadsJson);
+function onLocationFound(ll) {
+	var layer = leafletPip.pointInLayer(ll, quads);
+	var quad = layer[0].feature.properties.QUAD;
+	var pickupDay = quadToDay[quad];
 
-		onLocationFound(L.latLng(38.060403, -84.508001));
+	document.getElementById('day').innerHTML = layer.length ? '<h2>Your trash gets picked up on ' + pickupDay + '</h2>' : '';
+};
+
+$('#geocode').submit(function(e) {
+	var address = $('#formValueId').val();
+	console.log(address);
+	debugger;
+	var url = 'http://nominatim.openstreetmap.org/search?q='+ encodeURIComponent(address) +'&format=json&limit-1&addressdetails=1';
+	console.log(url);
+	debugger;
+	$.getJSON(url, function(data) {
+		console.log(data);
+	});
+	e.preventDefault;
+});
+
+// onLocationFound(L.latLng(38.060403, -84.508001));

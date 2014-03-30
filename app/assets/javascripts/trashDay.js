1df37223c76790ca5f17d6b04e0ec169a4b96b2e
Erik Schwartz <erik@erikschwartz.net>
2014-03-30 10:35:58 -0700

    Fix bootstrap error that broke dom ready

--- app/assets/javascripts/trashDay.js
+++ app/assets/javascripts/trashDay.js
@@ -1,33 +1,35 @@
-var quadToDay = {
-	"A": "Monday",
-	"B": "Thursday",
-	"C": "Tuesday",
-	"D": "Friday",
-	"E": "Daily"
-};
+$(function() {
+  var quadToDay = {
+    "A": "Monday",
+    "B": "Thursday",
+    "C": "Tuesday",
+    "D": "Friday",
+    "E": "Daily"
+  };
 
-var quads = L.geoJson(quadsJson);
+  var quads = L.geoJson(quadsJson);
 
-function onLocationFound(ll) {
-	var layer = leafletPip.pointInLayer(ll, quads);
-	if (layer.length) {
-		var quad = layer[0].feature.properties.QUAD;
-		var pickupDay = quadToDay[quad];
-	};
+  function onLocationFound(ll) {
+    var layer = leafletPip.pointInLayer(ll, quads);
+    if (layer.length) {
+      var quad = layer[0].feature.properties.QUAD;
+      var pickupDay = quadToDay[quad];
+    };
 
-	document.getElementById('day').innerHTML = layer.length ? '<h2>Your trash gets picked up on ' + pickupDay + '</h2>' : '<h2>Address outside of urban service area.</h2>';
-};
+    document.getElementById('day').innerHTML = layer.length ? '<h2>Your trash gets picked up on ' + pickupDay + '</h2>' : '<h2>Address outside of urban service area.</h2>';
+  };
 
-$('#geocode').submit(function(e) {
-	var address = $('#formValueId').val();
-	var url = 'http://nominatim.openstreetmap.org/search?q='+ encodeURIComponent(address) +'&format=json&limit-1&addressdetails=1';
-	$.getJSON(url, function(data) {
-		var lat = data[0].lat;
-		var lon = data[0].lon;
+  $('#geocode').submit(function(e) {
+    var address = $('#formValueId').val();
+    var url = 'http://nominatim.openstreetmap.org/search?q='+ encodeURIComponent(address) +'&format=json&limit-1&addressdetails=1';
+    $.getJSON(url, function(data) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
 
-		document.getElementById('coords').innerHTML = '<h2>Your address is located at [' + lat + ', ' + lon + '].</h2>';
+      document.getElementById('coords').innerHTML = '<h2>Your address is located at [' + lat + ', ' + lon + '].</h2>';
 
-		onLocationFound(L.latLng(lat, lon));
-	});
-	e.preventDefault();
+      onLocationFound(L.latLng(lat, lon));
+    });
+    e.preventDefault();
+  });
 });

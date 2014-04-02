08ab0f8d6916881aea1b5845d663bbf054a0b119
Erik Schwartz <erik@erikschwartz.net>
2014-04-01 17:54:33 -0700

    Use html5 validation, geocode on submit

--- app/assets/javascripts/citizen_validator.js
+++ app/assets/javascripts/citizen_validator.js
@@ -1,19 +1,19 @@
-$(function() {
-  var validator = new FormValidator('citizenForm', [{
-    name: 'citizen[address][mobile_number]',
-    rules: 'required'
-  }, {
-    name: 'citizen[address][street]',
-    rules: 'required'
-  }, {
-    name: 'citizen[email]',
-    rules: 'valid_email|required'
-  }], function(errors, event) {
-    if (errors.length > 0) {
-      $('.js-message-zone').html('<div class="alert alert-danger">Please correct errors below</div>');
-  }
-    errors.forEach(function(error) {
-      $('input[name="' + error.name + '"]').parent('.form-group').addClass('has-error');
-    });
-  });
-});
+// $(function() {
+//   var validator = new FormValidator('citizenForm', [{
+//     name: 'citizen[address][mobile_number]',
+//     rules: 'required'
+//   }, {
+//     name: 'citizen[address][street]',
+//     rules: 'required'
+//   }, {
+//     name: 'citizen[email]',
+//     rules: 'valid_email|required'
+//   }], function(errors, event) {
+//     if (errors.length > 0) {
+//       $('.js-message-zone').html('<div class="alert alert-danger">Please correct errors below</div>');
+//   }
+//     errors.forEach(function(error) {
+//       $('input[name="' + error.name + '"]').parent('.form-group').addClass('has-error');
+//     });
+//   });
+// });
--- app/assets/javascripts/trashDay.js
+++ app/assets/javascripts/trashDay.js
@@ -17,13 +17,16 @@ $(function() {
     };
 
     $('input#pickup').val(pickupDay);
-    document.getElementById('day').innerHTML = layer.length ? '<h2>Your trash gets picked up on ' + pickupDay + '</h2>' : '<h2>Address outside of urban service area.</h2>';
+    // document.getElementById('day').innerHTML = layer.length ? '<h2>Your trash gets picked up on ' + pickupDay + '</h2>' : '<h2>Address outside of urban service area.</h2>';
   };
 
-  $('#geocode').click(function(e) {
+  $('#citizenForm').submit(function(e) {
+    e.preventDefault();
+
     var address = $('#address').val();
     address += ' Lexington, KY'
     var url = 'http://nominatim.openstreetmap.org/search?q='+ encodeURIComponent(address) +'&format=json&limit-1&addressdetails=1';
+    var _this = this;
     $.getJSON(url, function(data) {
       var lat = data[0].lat;
       var lon = data[0].lon;
@@ -31,10 +34,10 @@ $(function() {
       $('#lat').val(lat);
       $('#lon').val(lon);
 
-      document.getElementById('coords').innerHTML = '<h2>Your address is located at [' + lat + ', ' + lon + '].</h2>';
+      // document.getElementById('coords').innerHTML = '<h2>Your address is located at [' + lat + ', ' + lon + '].</h2>';
 
       onLocationFound(L.latLng(lat, lon));
+      _this.submit();
     });
-    e.preventDefault();
   });
 });
